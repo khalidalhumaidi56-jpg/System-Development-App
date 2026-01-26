f34ac067cfc4dccbb3597206929df7e27d136f18
khalidalhumaidi56-jpg <khalidalhumaidi56@gmail.com>
2026-01-26 23:00:54 +0300

    Commit all remaining changes after SQLite switch

--- db.py
+++ db.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Add this line - this is what models.py is looking for
 Base = declarative_base()
 
 user = os.getenv("DB_USER")
@@ -10,11 +9,7 @@ password = os.getenv("DB_PASS")
 db_name = os.getenv("DB_NAME")
 host = os.getenv("DB_HOST")
 
-if host and host.startswith('/cloudsql'):
-    db_url = f"mysql+pymysql://{user}:{password}@/{db_name}?unix_socket={host}"
-else:
-    local_host = host if host else "127.0.0.1"
-    db_url = f"mysql+pymysql://{user}:{password}@{local_host}/{db_name}"
+db_url = "sqlite:///./app.db"
 
 engine = create_engine(db_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
